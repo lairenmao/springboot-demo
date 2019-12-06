184eeb40137e7a3175571883a997d1ed126ec9d8
lairenmao <2767323236@qq.com>
2019-12-06 17:33:05 +0800

    add controller

--- src/main/java/cn/wind/jenkinsdemo/TestController.java
+++ src/main/java/cn/wind/jenkinsdemo/TestController.java
@@ -1,4 +1,13 @@
 package cn.wind.jenkinsdemo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @RequestMapping("/jenkins/test")
+    public String test(){
+        return "hello world";
+    }
+
 }
