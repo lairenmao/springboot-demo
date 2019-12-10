685828b8a2f04f0909453a43b1f12a9a6e504146
lairenmao <2767323236@qq.com>
2019-12-10 11:31:33 +0800

    update

--- src/main/java/cn/wind/jenkinsdemo/controller/TestController.java
+++ src/main/java/cn/wind/jenkinsdemo/controller/TestController.java
@@ -1,13 +1,37 @@
 package cn.wind.jenkinsdemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class TestController {
     @RequestMapping("/jenkins/test")
     public String test(){
         return "hello world";
+    }
+    @PostMapping("/get/typeIds")
+    public void test2(@RequestParam("ids") int[] ids){
+        System.out.println(Arrays.toString(ids));
+
+    }
+    @PostMapping("/get/ids")
+    public void test3(@RequestBody int[] ids){
+        System.out.println(Arrays.toString(ids));
+
+    }
+    @PostMapping("/get/list")
+    public void test4(@RequestBody List<Integer> ids){
+        System.out.println(ids);
+    }
+    @PostMapping(value = "/str", consumes = "application/json")
+    @ResponseBody
+    public String str(@RequestParam("name") String aa,@RequestBody String name){
+        System.out.println(aa);
+        System.out.println(name);
+        return "success";
     }
 
 }
